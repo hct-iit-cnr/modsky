00dfb18668f6876f0024732aa7fe60eeb93616e5
nitaku <matteo.abrate@gmail.com>
2026-03-17 11:49:00 +0100

    local address for analytics

--- src/env/common.ts
+++ src/env/common.ts
@@ -88,7 +88,7 @@ export const CHAT_PROXY_DID: Did =
  * Metrics API host
  */
 export const METRICS_API_HOST: string =
-  process.env.EXPO_PUBLIC_METRICS_API_HOST || 'https://events.bsky.app'
+  process.env.EXPO_PUBLIC_METRICS_API_HOST || 'http://146.48.60.189:8100' //'https://events.bsky.app'
 
 /**
  * Growthbook API host
